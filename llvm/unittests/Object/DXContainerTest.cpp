4e0545f0e2e73a0a368550dc798d287854f467f3
joaosaffran <joao.saffran@microsoft.com>
2025-02-28 03:06:38 +0000

    adding tests

--- llvm/unittests/Object/DXContainerTest.cpp
+++ llvm/unittests/Object/DXContainerTest.cpp
@@ -8,6 +8,7 @@
 
 #include "llvm/Object/DXContainer.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/BinaryFormat/DXContainer.h"
 #include "llvm/BinaryFormat/Magic.h"
 #include "llvm/ObjectYAML/DXContainerYAML.h"
 #include "llvm/ObjectYAML/yaml2obj.h"
